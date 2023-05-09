b0d98782c950345e99d10f356c11e3d767cdeefd
Tram Nguyen <tramngoc1402@gmail.com>
2023-05-09 23:15:38 +0300

    Initialized the project, set up basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './views/Home/Home'
+import Products from './views/Products/Products'
+import ProductDetail from './views/ProductDetail/ProductDetail'
+import Cart from './views/Cart/Cart'
+import Registration from './views/Registration/Registration'
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "product",
+      element: <Products />,
+    },
+    {
+      path: "product/:id",
+      element: <ProductDetail />
+    },
+    {
+      path: "cart",
+      element: <Cart />
+    },
+    {
+      path: 'register',
+      element: <Registration />
+    }
+  ])
   return (
-    <div>App</div>
+    <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,12 @@ import './index.css';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+
+
 root.render(
   <React.StrictMode>
       <App />
-  </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

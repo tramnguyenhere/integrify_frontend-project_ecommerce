7bdfa1db7cac00f1568d7b33a4da94704d772aad
Tram Nguyen <tramngoc1402@gmail.com>
2023-05-14 23:36:29 +0300

    Adjusted code and style of products page

--- src/views/Products/Products.tsx
+++ src/views/Products/Products.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import useAppSelector from '../../hooks/useAppSelector'
 import ProductCard from '../../components/ProductCard/ProductCard'
